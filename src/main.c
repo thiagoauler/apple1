aff8e019ec81539499f2171d32dfe0f6674e7f7a
Thiago Auler <thiagoauler@users.noreply.github.com>
2017-11-20 12:32:03 -0200

    creating interface i/o functions

--- src/main.c
+++ src/main.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
 #include "inc/6502.h"
 
 int main(int argc, char **argv)
 {
-    setbuf(stdout, NULL);
-    printf("Apple-I Computer\n");
-    
     init(); // start the processor
     run();  // and runs it
     
